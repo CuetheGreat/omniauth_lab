2d8be5d76b0c990fcfe5e1f2b9cbe84f5da5af9b
Clay Stewart <c.stewart89@gmail.com>
2022-02-09 21:58:37 -0500

    commit tree 83c2e7a6b9f5aa9ec868f205ee30b75a4aa25613

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
-Rails.application.routes.draw do
-  # Add your routes here
-end
+Rails
+  .application
+  .routes
+  .draw do
+    # Add your routes here
+    get '/', to: 'welcome#home', as: :root
+    post '/logout', to: 'sessions#destroy'
+
+    match '/auth/:provider/callback', to: 'sessions#create', via: %i[get post]
+  end
